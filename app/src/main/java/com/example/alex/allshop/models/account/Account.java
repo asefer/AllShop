d818608e3f12664ab7bccc00317cd4229410c360
alex <lexa.zule7@gmail.com>
2017-12-12 20:25:55 +0200

    first commit

--- app/src/main/java/com/example/alex/allshop/models/account/Account.java
+++ app/src/main/java/com/example/alex/allshop/models/account/Account.java
@@ -1,8 +1,12 @@
 package com.example.alex.allshop.models.account;
 
+import android.util.Base64;
+
 /**
  * Created by alex on 12.12.17.
  */
 
-public class Account {
+public class Account  {
+    private String name;
+    private Base64 password;
 }
