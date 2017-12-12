dc6e7b4e7e56205b74e388179c49fb0781809cf7
alex <lexa.zule7@gmail.com>
2017-12-12 20:25:37 +0200

    first commit

---
+++ app/src/main/java/com/example/alex/allshop/models/account/Account.java
@@ -0,0 +1,8 @@
+package com.example.alex.allshop.models.account;
+
+/**
+ * Created by alex on 12.12.17.
+ */
+
+public class Account {
+}
---
+++ app/src/main/java/com/example/alex/allshop/presentation/MainFragment.java
@@ -0,0 +1,8 @@
+package com.example.alex.allshop.presentation;
+
+/**
+ * Created by alex on 12.12.17.
+ */
+
+public class MainFragment {
+}
